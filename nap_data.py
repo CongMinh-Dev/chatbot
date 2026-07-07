ce852b6366357aac50cc9a72136adbf919955a63
Minh <lightstormlxag93@gmail.com>
2026-07-07 22:59:13 +0700

    .

--- nap_data.py
+++ nap_data.py
@@ -5,6 +5,7 @@ from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
 from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 from dotenv import load_dotenv
 
@@ -64,13 +65,19 @@ def ingest_data():
     load_dotenv()
     NVIDIA_API_KEY = os.getenv("NVIDIA_API_KEY")
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-    embeddings = NVIDIAEmbeddings(
-        model="nvidia/nv-embed-v1", # Hoặc model phù hợp khác
-        api_key=NVIDIA_API_KEY
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    
+    # embeddings = NVIDIAEmbeddings(
+    #     model="nvidia/nv-embed-v1", # Hoặc model phù hợp khác
+    #     api_key=NVIDIA_API_KEY
+    # )
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/gemini-embedding-001",
+        google_api_key=GOOGLE_API_KEY,
     )
-    # embeddings = GoogleGenerativeAIEmbeddings(
-    #     model="models/gemini-embedding-001",
-    #     google_api_key=GOOGLE_API_KEY,
+    # embeddings = OpenAIEmbeddings(
+    #     model="text-embedding-3-small",
+    #     api_key=OPENAI_API_KEY
     # )
     
     # 4. Khởi tạo VectorStore và lưu thủ công từng đoạn để tránh treo
